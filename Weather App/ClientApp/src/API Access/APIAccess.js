8675842a67a8498b7161c7520e8047253fd8737a
SpicyMike <MDMoore1764@gmail.com>
2020-11-12 17:58:46 -0500

    Edit URLS for local MVC

--- Weather App/ClientApp/src/API Access/APIAccess.js
+++ Weather App/ClientApp/src/API Access/APIAccess.js
@@ -1,26 +1,22 @@
-  export const GetForecastData = async (Lat, Long, setLoadFailed) => {
-    const data = await fetch(`http://localhost:5000/forecast?latitude=${Lat}&longitude=${Long}`, {mode: 'cors'}).then(res => res.json()).catch(() => setLoadFailed(true));
+export const GetForecastData = async (Lat, Long, setLoadFailed) => {
+    const data = await fetch(`forecast?latitude=${Lat}&longitude=${Long}`, { mode: 'cors' }).then(res => res.json()).catch(() => setLoadFailed(true));
+
     return data;
-  }
+}
 
 
-  export const GetCoordinateData = async (address, setLoadFailed) =>{
+export const GetCoordinateData = async (address, setLoadFailed) => {
 
-    if(address.Street === "")
-    {
-      return null;
+    if (address.Street === "") {
+        return null;
     }
-    else if(address.Zip === "" && (address.City === "" || address.State === "") )
-    {
-      return null;
+    else if (address.Zip === "" && (address.City === "" || address.State === "")) {
+        return null;
     }
-    else
-    {
-      //We know we have all required parts.
-      const data = await fetch(`http://localhost:5000/coordinate?street=${address.Street}&city=${address.City}&state=${address.State}&zip=${address.Zip}`, {mode: 'cors'}).then(res => res.json()).catch(() => setLoadFailed(true));
-      return data;
-      // return data;
-    }
-  }
+    else {
+        const data = await fetch(`coordinate?street=${address.Street}&city=${address.City}&state=${address.State}&zip=${address.Zip}`, { mode: 'cors' }).then(res => res.json()).catch(() => setLoadFailed(true));
 
-  
+        return data;
+    }
+}
+
